b24b9439bb879c499b63ec0b5c1f553b1d3a0315
BinaryConvertz <realcoder2023@gmail.com>
2026-01-14 14:28:44 -0500

    commited code

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,4 @@
-// "use client";
-
 import axios from "axios";
-// import DataFetched from "./Components/DataFetched";
 import Header from "./Components/Header";
 import { useEffect, useState } from "react";
 
